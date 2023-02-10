b2ca8b1704859147be20cd57b521672ec0c06e5e
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-11 00:07:30 +0300

    navbar component imported to app js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Navbar } from "components";
 import { Home } from "pages";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
