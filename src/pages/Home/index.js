f1bc9765c1926d4646a438971a3145a141d8290a
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-11 22:40:55 +0300

    home pages created

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,21 @@
+import { Hero, Info } from "components";
 import React from "react";
-
+import Center from "../../assets/images/center.jpg";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section>
+        <Hero />
+
+        <Info />
+        <div className="center">
+          <div className="center__container">
+            <img src={Center} alt="" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
