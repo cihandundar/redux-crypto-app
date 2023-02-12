9ef2b7d4218d145815936bc8985e9d29ddc63ce1
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-12 14:27:19 +0300

    fix

---
+++ src/pages/CoinList/index.js
@@ -0,0 +1,12 @@
+import { CoinItem } from "components";
+import React from "react";
+
+const CoinList = () => {
+  return (
+    <div>
+      <CoinItem />
+    </div>
+  );
+};
+
+export default CoinList;
