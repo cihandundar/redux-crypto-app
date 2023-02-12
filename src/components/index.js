48a66c4d307bf628fd491d95df60d0c4f48b1010
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-12 21:09:34 +0300

    chart imported

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Info from "./Info";
 import CoinItem from "./CoinItem";
-export { Navbar, Hero, Info, CoinItem };
+import HistoryChart from "./HistoryChart";
+export { Navbar, Hero, Info, CoinItem, HistoryChart };
