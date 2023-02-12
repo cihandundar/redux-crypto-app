e84def23381226140ac78011e2611e5daaa24ead
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-02-12 21:09:47 +0300

    detail page added

--- src/pages/CoinDetails/index.js
+++ src/pages/CoinDetails/index.js
@@ -1,3 +1,4 @@
+import { HistoryChart } from "components";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -15,10 +16,71 @@ const CoinDetails = () => {
   console.log(data);
 
   return (
-    <div className="details">
-      <div className="details__container">
-        <div className="details__wrapper">
-          <div className="details__container__box"></div>
+    <div className="detail">
+      <div className="detail__container">
+        <div className="detail__wrapper">
+          <div className="detail__container__box">
+            <div className="detail__container__box__rank">
+              <h4>#Rank: {data?.market_data?.market_cap_rank}</h4>
+            </div>
+            <div className="detail__container__box__title">
+              <div className="detail__container__box__img">
+                <img src={data?.image?.large} alt="" />
+              </div>
+              <h3>{data?.name}</h3>
+              <span>{data?.symbol}</span>
+            </div>
+            <div className="detail__container__box__wrapper">
+              <ul className="detail__container__box__list">
+                <li
+                  className={
+                    data?.market_data?.price_change_percentage_24h < 0
+                      ? `detail__container__box__link detail__container__box__link--down `
+                      : `detail__container__box__link detail__container__box__link--up`
+                  }
+                >
+                  <h3>24h</h3>
+                  {data?.market_data?.price_change_percentage_24h}
+                </li>
+                <li
+                  className={
+                    data?.market_data?.price_change_percentage_7d < 0
+                      ? `detail__container__box__link detail__container__box__link--down `
+                      : `detail__container__box__link detail__container__box__link--up`
+                  }
+                >
+                  <h3>7d</h3>
+                  {data?.market_data?.price_change_percentage_7d}
+                </li>
+                <li
+                  className={
+                    data?.market_data?.price_change_percentage_14d < 0
+                      ? `detail__container__box__link detail__container__box__link--down `
+                      : `detail__container__box__link detail__container__box__link--up`
+                  }
+                >
+                  <h3>14d</h3>
+                  {data?.market_data?.price_change_percentage_14d}
+                </li>
+                <li
+                  className={
+                    data?.market_data?.price_change_percentage_30d < 0
+                      ? `detail__container__box__link detail__container__box__link--down `
+                      : `detail__container__box__link detail__container__box__link--up`
+                  }
+                >
+                  <h3>30d</h3>
+                  {data?.market_data?.price_change_percentage_30d}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="detail__text">
+          <p dangerouslySetInnerHTML={{ __html: data?.description?.en }}></p>
+        </div>
+        <div className="detail__chart">
+          <HistoryChart />
         </div>
       </div>
     </div>
